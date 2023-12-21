8a0116768317559fba789220d11019dcec836752
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2023-12-20 23:32:35 -0500

    btn click going to new screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,8 +1,28 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>Welcome to the Future!</Text>;
+const HomeScreen = ({navigation}) => {
+ // console.log(props)
+  return (
+    <View>
+      <Button
+        title="Go to Component Screen"
+        onPress={() => {
+          navigation.navigate("Components")
+          console.log("going to component screen")
+        }
+        } />
+      <Button
+        title="Go to List Screen"
+        onPress={() => {
+          navigation.navigate("ListScreen");
+          console.log("going to list screen")
+        }} />
+      {/* <TouchableOpacity onPress={()=> console.log("list clicked")}>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity> */}
+    </View>  
+  )
 };
 
 const styles = StyleSheet.create({
